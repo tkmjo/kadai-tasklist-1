d7261ca0af6eae0f90e7b0b6a4415f113ed95bba
tkmjo <t.kamijo87395426@gmail.com>
2018-04-30 18:05:37 +0000

    CRUD tasks

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>新規タスク追加ページ</h1>
+    
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content') !!}
+        
+        {!! Form::submit('投稿') !!}
+    
+    {!! Form::close() !!}
+
+@endsection
