9d896d196c578f539774ebc80b1bc852c9a86f4f
tkmjo <t.kamijo87395426@gmail.com>
2018-05-04 04:12:29 +0000

    modified tasklist

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,20 +4,18 @@
 
     <h1>新規タスク追加ページ</h1>
     
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    <div class="from-group col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+        {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-        <div class="from-group col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             {!! Form::label('status', '状態:') !!}
             {!! Form::text('status', null, ['class' => 'form-control']) !!}
-        </div>
         
-        <div class="from-group col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             {!! Form::label('content', 'タスク:') !!}
             {!! Form::text('content', null, ['class' => 'form-control']) !!}
-        </div>    
         
             {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
-    {!! Form::close() !!}
+        {!! Form::close() !!}
+    </div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,20 +4,17 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
     
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    <div class="form-group ol-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+        {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
         
-        <div class="form-group ol-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::label('status', '状態:') !!}
             {!! Form::text('status', null, ['class' => 'form-control']) !!}
-        </div>
         
-        <div class="form-group col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::label('content', 'タスク:') !!}
             {!! Form::text('content', null, ['class' => 'form-control']) !!}
-        </div>
         
         {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
     
-    {!! Form::close() !!}
-
+        {!! Form::close() !!}
+    </div>
 @endsection
